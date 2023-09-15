93db418f602d636fa0a3c3d6ea898b21c6f2f833
xiajieqiong <xiajieqiong@tuzhanai.com>
2023-09-15 11:14:29 +0800

    fix sdk

--- sdk/statistic.js
+++ sdk/statistic.js
@@ -10,15 +10,19 @@ function visit() {
     const ntype = window.performance.navigation.type;
     if (ntype !== 0) return;
   }
-  const referer = window.referer;
+  const referrer = document.referrer;
   const ua = window.navigator.userAgent;
   const hostname = window.location.hostname;
   window.fetch('https://si.xjq.icu/v1/visit', {
-    body: {
-      referer,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      referrer,
       ua,
       app_id: hostname,
-    },
+    }),
   });
 }
 visit();
